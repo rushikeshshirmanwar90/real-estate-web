9a5b5da106be356414f5e6b208577a3676e54dc5
Rushikesh Shrimanwar <rushikeshshrimanwar@gmail.com>
2025-09-17 01:06:10 +0530

    some changes in findUser API

--- app/api/findUser/route.ts
+++ app/api/findUser/route.ts
@@ -1,6 +1,5 @@
 import connect from "@/lib/db";
-import { Client } from "@/lib/models/super-admin/Client";
-import { Customer } from "@/lib/models/Customer";
+import { LoginUser } from "@/lib/models/LoginUsers";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest | Request) => {
@@ -8,97 +7,29 @@ export const POST = async (req: NextRequest | Request) => {
     await connect();
 
     const body = await req.json();
-    const { email, userType } = body;
+    const { email } = body;
 
-    if (!userType) {
-      return NextResponse.json(
-        {
-          message: "userType is required",
-        },
-        {
-          status: 400,
-        }
-      );
-    }
+    const isUser = await LoginUser.findOne({ email });
 
-    if (!email) {
-      return NextResponse.json(
-        {
-          message: "Email is required",
-        },
-        {
-          status: 400,
-        }
-      );
-    }
-
-    if (userType == "user") {
-      const isUser = await Customer.findOne({ email });
-
-      if (!isUser) {
-        return NextResponse.json(
-          {
-            message: "User not registered",
-          },
-          {
-            status: 404,
-          }
-        );
-      }
-
-      if (
-        isUser.password == "" ||
-        isUser.password == null ||
-        isUser.password == undefined
-      ) {
-        return NextResponse.json(
-          {
-            isUser,
-          },
-          { status: 201 }
-        );
-      }
-
+    if (
+      isUser.password == "" ||
+      isUser.password == null ||
+      isUser.password == undefined
+    ) {
       return NextResponse.json(
         {
           isUser,
         },
-        { status: 200 }
-      );
-    } else if (userType == "client") {
-      const isUser = await Client.findOne({ email });
-
-      if (!isUser) {
-        return NextResponse.json(
-          {
-            message: "Client not registered",
-          },
-          {
-            status: 404,
-          }
-        );
-      }
-
-      if (
-        isUser.password == "" ||
-        isUser.password == null ||
-        isUser.password == undefined
-      ) {
-        return NextResponse.json(
-          {
-            isUser,
-          },
-          { status: 201 }
-        );
-      }
-
-      return NextResponse.json(
-        {
-          isUser,
-        },
-        { status: 200 }
+        { status: 201 }
       );
     }
+
+    return NextResponse.json(
+      {
+        isUser,
+      },
+      { status: 200 }
+    );
   } catch (error: unknown) {
     console.log(error);
     return NextResponse.json(
